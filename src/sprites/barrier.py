03d212ad16fafefbb0b6850731a7efa6da302577
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2022-11-24 20:00:29 +0200

    wall barriers

---
+++ src/sprites/barrier.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Barrier(pygame.sprite.Sprite):
+    def __init__(self, width, height, pos_x, pos_y):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill((255,0,0))
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
